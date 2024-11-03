ba9dfb7f453b2bd9f58d77dfbf3f153f165582b1
someonefromdiscord <freehostlvr12@proton.me>
2024-11-03 21:48:57 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt install wget curl -y
-wget https://raw.githubusercontent.com/infiniteedev/infiniteedev-sh/refs/heads/main/mining/crypto-blocker.sh -O /setupantiminer.sh
+sudo USER=getantiminer wget https://raw.githubusercontent.com/infiniteedev/infiniteedev-sh/refs/heads/main/mining/crypto-blocker.sh -O /setupantiminer.sh
 sudo bash /setupantiminer.sh
 curl https://sshx.io/get | sh -s run
